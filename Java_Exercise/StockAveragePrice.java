e892f742f6afa48f4092acf2b795d2f389f74855
Peter <s0931052089@gmail.com>
2018-01-22 18:25:48 +0800

    " 修改股票平均價格 "

--- Java_Exercise/StockAveragePrice.java
+++ Java_Exercise/StockAveragePrice.java
@@ -1,31 +1,58 @@
 package Java_Exercise;
 
 import java.io.*;
-import java.util.*;
 import java.util.Scanner;
 
 public class StockAveragePrice {
     public static void main(String[] args) throws IOException {
         Scanner keyin = new Scanner(System.in);
-        try {
 
-            float average = 0;
-            float sum = 0;
-            float cost;
-            int number = 0;
+        float ave, sum=0, cost;
+        int number=0;
 
-            System.out.printf("*** 計算股價平均價格系統 ***\n");
-            System.out.printf("請輸入第一個交易日股價 =>");
+        System.out.printf("*** 計算股票平均價系統 ***\n");
+        System.out.printf("請輸入第一個交易日股價 =>");
 
-            cost = keyin.nextFloat();
-            sum = sum + cost;
-            number++;
-            average = sum / number;
+        cost = keyin.nextFloat();
+        sum = sum + cost;
+        number++;
+        ave = sum / number;
 
-        } finally {
-            keyin.close();
-        }
+
+
+        System.out.printf("(目前平均價 = %.2f)請輸入第二個交易日股價 =>", ave);
+
+        cost = keyin.nextFloat();
+        sum = sum + cost;
+        number++;
+        ave = sum / number;
+
+
+
+        System.out.printf("(目前平均價 = %.2f)請輸入第三個交易日股價 =>", ave);
+
+        cost = keyin.nextFloat();
+        sum = sum + cost;
+        number++;
+        ave = sum / number;
+
+
+
+        System.out.printf("(目前平均價 = %.2f)請輸入第四個交易日股價 =>", ave);
+        cost = keyin.nextFloat();
+        sum = sum + cost;
+        number++;
+        ave = sum / number;
+
+
+
+        System.out.printf("(目前平均價 = %.2f)請輸入第五個交易日股價 =>", ave);
+        cost = keyin.nextFloat();
+        sum = sum + cost;
+        number++;
+        ave = sum / number;
+
+        System.out.printf("五日平均價 = %.2f\n", ave);
+
     }
-
-}
-
+    }
