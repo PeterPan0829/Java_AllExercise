4b6de44b0aea95bf8f1049b147e19ae72b54b2bd
Peter <s0931052089@gmail.com>
2018-01-22 14:02:27 +0800

    新增所有 Java 檔案

---
+++ Java_Exercise/Main.java
@@ -0,0 +1,8 @@
+package Java_Exercise;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ Java_Exercise/Test.java
@@ -0,0 +1,7 @@
+package Java_Exercise;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.printf("測試用～～");
+    }
+}
