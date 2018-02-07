65375a24309f88c6172759443a153b1232ae410a
Peter <s0931052089@gmail.com>
2018-02-07 21:28:47 +0800

    " 新增一系列累加程式變化_for "

---
+++ Java_FlowControl/AccumulatorChange.java
@@ -0,0 +1,57 @@
+package Java_FlowControl;
+
+/*
+    此為一系列累加程式變化
+
+    ----Review----
+    count += 2
+    count = count + 2
+
+ */
+
+public class AccumulatorChange {
+    public static void main(String[] args) {
+
+//        (1)功能：修改 Ex5_1.java 程式中一個地方，使其功能為 sum = 1 + 2, …, 1000，執行結果如下：
+//        + 2 + .. + 1000 = 500500
+//        int sum = 0, count;
+//
+//        for (count=1; count <=1000; count=count+1){
+//            sum = sum + count;
+//        }
+//        System.out.printf("1 + 2 + .. + 1000 = %d",sum);
+//
+
+//        (2)功能：修改 Ex5_1.java，使其功能為 sum = 100 + 99 …, + 1，執行結果如下：
+//        100 + 99 + .. + 1 = 5050
+//
+//        int sum = 0, count;
+//
+//        for (count=1; count<101; count++){
+//            sum = sum + count;
+//        }
+//        System.out.printf("100 + 99 + .. + 1 = %d",sum);
+
+//        (3)功能：修改 Ex5_1.java，使其功能為 sum = 0 + 2 + 4 + … 1000，其結果如下：
+//        2 + 4 + 8 + ... + 1000 = 250500 只加偶數
+//
+//        int sum = 0, count;
+//
+//        for (count=2; count<=1000; count=count+2){
+//            sum = sum + count;
+//        }
+//        System.out.printf("0 + 2 + 4 + ... + 1000 = %d",sum);
+
+
+//        功能：修改 Ex5_1.java，使其功能為 sum = 1 + 3 + 5 +,…, 999，執行結果如下：
+//        1 + 3 + .. + 999 = 250000 只加奇數
+//        int sum = 0, count;
+//
+//        for (count=1; count<=999; count = count + 2){
+//            sum += count;
+//        }
+//        System.out.printf("1 + 3 + 5 + ... + 999 = %d",sum);
+
+    }
+}
+
