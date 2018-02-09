b0aa823a5b2684a493ee65fe34c0a77bb1b11e71
Peter <s0931052089@gmail.com>
2018-02-09 11:18:23 +0800

    " 修改累加/累乘程式_while "

--- Java_FlowControl/MultiplicationTable.java
+++ Java_FlowControl/MultiplicationTable.java
@@ -1,5 +1,13 @@
 package Java_FlowControl;
 
+/*
+    九九乘法表測試，系統會連續出現 10 題兩個 1 ~ 9 之間亂數，學生輸入自己計算相乘的結果，最後評定學生得分（每題 10 分）。
+
+    系統要求連續出現 10 次九九乘法練習題，可利用 for 迴圈達成，其中，計數器（count）由 1 到 10，每次增量 1（count++，或 count = count + 1）。
+    迴圈內必須產生兩個 1 ~ 9 的亂數需要導入 java.util.Random 套件
+ */
+
+
 import java.util.Random;
 import java.util.Scanner;
 
@@ -9,7 +17,7 @@ public class MultiplicationTable {
         Random ran = new Random();
 
         int value1, value2, total, count;
-        int answer, correct = 0;
+        int answer, correct = 0, grade = 0;
 
 
         for (count=1; count<=10; count++){
@@ -22,12 +30,14 @@ public class MultiplicationTable {
 
             answer = new Scanner(System.in).nextInt();
 
+            grade = correct * 10;
+
             if (answer == total){
                 correct = count + 1;
             }else {
-                System.out.printf("\n答錯了, 正確答案為：%d",total);
+                System.out.printf("\n答錯了, 正確答案為：%d\n",total);
             }
         }
-        System.out.printf("你的成績為：%d * 10",correct);
+        System.out.printf("你的成績為：%d",grade);
     }
 }
