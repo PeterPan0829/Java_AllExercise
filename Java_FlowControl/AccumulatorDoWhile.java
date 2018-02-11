275c8e587f5a6309a534692f0ec7bf6223c9c24f
Peter <s0931052089@gmail.com>
2018-02-11 15:54:45 +0800

    " 新增連續累加程式_while "

---
+++ Java_FlowControl/AccumulatorDoWhile.java
@@ -0,0 +1,15 @@
+package Java_FlowControl;
+
+public class AccumulatorDoWhile {
+    public static void main(String[] args) {
+        int sum = 0, limit=100, count;
+        count = limit;
+
+        do {
+            sum = sum + count;
+            count = count - 1;
+
+        }while (count > 0);
+        System.out.printf("\n%d + 99 + 98 ..... + 1 = %d",limit,sum);
+    }
+}
