820bd6e02c9d92b8a33015c2fcbc57ea70335a07
peter <s0931052089@gmail.com>
2018-02-20 11:58:28 +0800

    " 新增心算過五關遊戲_運用break（未完成）  "

---
+++ Java_FlowControl/MentalArithmetic.java
@@ -0,0 +1,36 @@
+//package Java_FlowControl;
+//
+//import java.util.Random;
+//import java.util.Scanner;
+//
+//public class MentalArithmetic {
+//    public static void main(String[] args) {
+//        Scanner keyin = new Scanner(System.in);
+//        Random random1 = new Random();
+//        Random random2 = new Random();
+//
+//        int base = 1;                                           //亂數
+//        int flag = 1;                                           //過五關旗號
+//        int count;
+//        int value;
+//
+//
+//        int ran1;
+//        int ran2;
+//
+//        for (count=1; count<=5; count++){
+//            base = base*5;
+//
+//            ran1 = random1.nextInt(base);
+//            ran2 = random2.nextInt(base);
+//            value = ran1 +
+//
+//        }
+//
+//
+//
+//
+//
+//
+//    }
+//}
