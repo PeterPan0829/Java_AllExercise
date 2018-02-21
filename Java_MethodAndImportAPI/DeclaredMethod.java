6760c810bd83f8de202fc2202aea1c008eca13d0
peter <s0931052089@gmail.com>
2018-02-21 23:35:35 +0800

    " 新增方法的宣告 "

---
+++ Java_MethodAndImportAPI/DeclaredMethod.java
@@ -0,0 +1,24 @@
+package Java_MethodAndImportAPI;
+
+/*
+    //更詳細的說明會直接在 Github wiki 分享, 請看完整的教學說明～
+    宣告方法 Method (函數) 範例
+ */
+
+public class DeclaredMethod {
+    public static void main(String[] args) { //宣告一個主方法 public static表示允許外部呼叫此類別的方法
+
+        //這裡是方法的實體
+        //void (空值) = 不需要return
+
+    }
+}
+
+
+//(1) 方法屬性：可被引用（呼叫）的屬性，常用有下列組合型態：
+//
+//        (a) public static：允許類別外部呼叫的靜態（或稱類別）方法。
+//
+//        (b) private static：僅允許類別內其他方法呼叫的靜態方法。
+//
+//        (c) static：如同 public static 屬性。
