ae59ab1335ff17256aede60e679f89b51f4e2c37
peter <s0931052089@gmail.com>
2018-02-21 16:26:50 +0800

    " 修改學貸償還試算系統(未完成)_dowhile "

--- Java_FlowControl/StudentLoan.java
+++ Java_FlowControl/StudentLoan.java
@@ -7,36 +7,38 @@ public class StudentLoan {
 
         /* 宣告相關物件與變數 */
         Scanner keyin = new Scanner(System.in);
-        double credit = 0;      // 需要貸款額度(萬)
+        double credit;          // 需要貸款額度(萬)
         double pay_month;       // 每月還錢金額(萬)
-        double pay;             // ?????????????????????
-        double sum = 0;             // 累加產生利息
+        double pay;             // 應繳金額
+        double sum = 0;         // 累加產生利息
         double rate;            // 年利率
-        double stages = 0;          // 還錢期數
+        double stages;          // 還錢期數
         double balance;         // 目前貸款餘額
         double interest;        // 本月延伸利息
         int count=1;            // 累計繳納期數
 
-        balance = credit;
-        pay_month = credit / stages;
 
         /* 讀取客戶資料 */
         System.out.printf("==============學貸償還試算系統==============");
         System.out.printf("\n請輸入貸款金額（萬）=>");
         credit = keyin.nextDouble();
-        System.out.printf("\n請輸入年利率 (如0.04) =>");
+        System.out.printf("請輸入年利率 (如0.04) =>");
         rate = keyin.nextDouble();
-        System.out.printf("\n請輸入就讀期間貸款學期數 =>");
+        System.out.printf("請輸入就讀期間貸款學期數 =>");
         stages = keyin.nextInt();
         System.out.printf("\n前 10 期應繳納金額如下：");
+        pay_month = credit / stages;                                       //每月平均分攤本金
 
+        balance = credit;                                                  //初始餘額
         do {
             interest = (balance * rate) / 12;                              //本月延伸利息
             pay = pay_month + interest;                                    //計算目前該月應繳金額
             balance = balance - pay_month;                                 //計算本月餘額
             count = count + 1;                                             //累加還款期數
             sum = sum + interest;                                          //累加產生利息
-            System.out.printf("\n第 %d 期需繳納 %f 萬元",count,pay);        //輸出本月期數、應繳金額
+//            credit / (stages * 12) + interest
+            System.out.printf("\n第 %d 期需繳納 %.3f 萬元",count,pay);        //輸出本月期數、應繳金額
+
         } while (count < stages);                                          //是否已到期
         System.out.printf("\n到清償為止，總共延伸利息 %f 萬元",interest);
     }
