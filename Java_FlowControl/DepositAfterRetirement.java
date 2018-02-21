91de2502911c584a43aff57696e18f969f8a0f36
peter <s0931052089@gmail.com>
2018-02-21 22:45:15 +0800

    " 新增預備退休生活（未完成） "

--- Java_FlowControl/DepositAfterRetirement.java
+++ Java_FlowControl/DepositAfterRetirement.java
@@ -17,11 +17,15 @@ public class DepositAfterRetirement {
     public static void main(String[] args) {
         Scanner keyin = new Scanner(System.in);
 
-        final int limit_age = 80;
-        int balance;
-        int credit = 0;
-        int month, retired_age, age, life;
-        double rate;
+        final int limit_age = 80;               //假設是人的最高壽命
+        int balance;                            //餘額
+        int credit;                             //每月儲蓄
+        int month;                              //總共月數
+        int retired_age;                        //退休年紀
+        int age;                                //開始儲蓄退休金年紀
+        int life;                               //每個月生活費
+        double rate;                            //利率
+//        int count = 1;
 
         System.out.printf("***    評估退休後生活品質系統   ***");
         System.out.printf("\n請輸入幾歲開始儲存退休金(如 30)歲 =>");
@@ -31,41 +35,38 @@ public class DepositAfterRetirement {
         System.out.printf("請輸入預估年利率(如 0.14) =>");
         rate = keyin.nextDouble();
         System.out.printf("請輸入預估每月儲蓄(如 10000)元 =>");
-        balance = keyin.nextInt();
+        credit = keyin.nextInt();
 
-        month = (retired_age - age) * 12;           //假設五十歲退休三十歲開始存錢且一年十二個月就是 50 - 35 = 15 * 12
-//        balance = credit;
+
+        month = (retired_age - age) * 12; //假設五十歲退休三十歲開始存錢且一年十二個月就是 50 - 35 = 15 * 12
+        balance = credit;
+
         for (int i=0; i<month; i++){
             balance = (int) (balance + (balance * rate/12));
             balance = balance + credit;
+//            count = count + 1;
         }
 
-
-        System.out.printf("\n到退休之前可儲存 %d 元\n",credit);
-
-        life = (limit_age - retired_age) * 12 ;
+        System.out.printf("\n到退休之前可儲存 %d 元\n",balance);
+        life = ((limit_age - retired_age) * 12) ;
         System.out.printf("平均每個月有 %d 元生活費",life);
 
-        if (credit<5000){
+
+        if (balance<5000){
             System.out.printf("等待救濟");
 
-         if (credit>5000 && credit<10000){
+         if (balance>5000 && balance<10000){
             System.out.printf("免強可以湖口");
          }
-         if (credit>10000 && credit<20000){
+         if (balance>10000 && balance<20000){
              System.out.printf("安逸舒適");
          }
-         if (credit>20000 && credit<40000){
+         if (balance>20000 && balance<40000){
              System.out.printf("富貴生活");
          }
-         if (credit > 40000){
+         if (balance > 40000){
              System.out.printf("周遊列國，遨遊世界");
          }
         }
     }
 }
-//            (1) 5000 元以下：等待救濟。
-//            (2) 5000 ~ 10000 元：免強可以湖口。
-//            (3) 10,000 ~ 20,000：安逸舒適。
-//            (4) 20,000 ~ 40,000：富貴生活。
-//            (5) 40,000 以上：遨遊世界。
