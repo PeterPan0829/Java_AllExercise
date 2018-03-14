97a2a03dadaab4a7376ef5d683dff207a709b41f
peter <s0931052089@gmail.com>
2018-03-15 01:25:05 +0800

    " 新增印出股票歷史價格 "

--- Java_ArrayDataStructure/PrintStockHistoryPrice.java
+++ Java_ArrayDataStructure/PrintStockHistoryPrice.java
@@ -2,10 +2,10 @@ package Java_ArrayDataStructure;
 
 public class PrintStockHistoryPrice {
     public static void main(String[] args) {
-        double[] course = {78.8, 72.3, 61, 56, 87, 66.3, 74.5, 88, 76, 58};
+        double[] StockPrice = {78.8, 72.3, 61, 56, 87, 66.3, 74.5, 88, 76, 58};
 
-        for (int i=0; i<course.length; i++){
-            System.out.printf("%.2f",course[i]);
+        for (int i=0; i<StockPrice.length; i++){
+            System.out.printf("%.2f",StockPrice[i]);
             System.out.printf("\t");
         }
 
