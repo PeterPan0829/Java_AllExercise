3a4dcfb603594a45181c560db7fadb7cd1fc0577
許家翔 <sjshiang@hotmail.com>
2018-05-07 16:17:13 +0800

    " 新增循序搜尋法（陣列搜尋基礎） "

--- Java_ArrayDataStructure/SequentialSearch.java
+++ Java_ArrayDataStructure/SequentialSearch.java
@@ -2,8 +2,7 @@ package Java_ArrayDataStructure;
 /*
 循序搜尋法(Sequential Search) 即 線性搜尋法(Linear Search)，
 從陣列內尋找某一筆資料，最簡單的方法是循序搜尋法；演算法是由陣列的起頭開始比較尋找（比較內容），
-如搜尋到立即停止，否則繼續往下一個元素尋找，一直到陣列結束為止。
-
+如果搜尋到立即停止，否則繼續往下一個元素尋找，一直到陣列結束為止。
 簡單來說就是直接用迴圈一個個去比對，找到時就跳出。
  */
 
@@ -29,12 +28,11 @@ public class SequentialSearch {
                 flag = 1;
                 break;
             }
-            i++;                                                                //還沒找到就繼續找
+            i++;                                                               //還沒找到就繼續找
         }
         if (flag ==1){
             System.out.printf("num[%d] = %d 找到囉兄弟！\n",i,value);            //前面的 i 是 num 陣列當中的第幾個位置
         }else
             System.out.printf("%d 不在 num 陣列內",value);
-        
     }
 }
