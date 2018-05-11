7d5f2caacbc14d6854786a892e7f732813578918
許家翔 <sjshiang@hotmail.com>
2018-05-12 01:21:48 +0800

    " 新增有序陣列刪除元素（運用二分搜尋法(BinarySearch)） "

--- Java_ArrayDataStructure/Array_Sequence_excute.java
+++ Java_ArrayDataStructure/Array_Sequence_excute.java
@@ -1,20 +1,21 @@
 package Java_ArrayDataStructure;
 
+
 import java.util.Scanner;
 
 public class Array_Sequence_excute {
 
-    static int num[] = new int[50];
-    static int point;
+    static int num[] = new int[50];                     // 宣告陣列空間
+    static int point;                                   // 宣告游標變數
 
     public static void main(String[] args) {
         Scanner keyin = new Scanner(System.in);
         point = -1;                                     //設置空值
         int select, k, item;                            //插入元素 item
 
-        for (int i=0; i<50; i++){
-            num[i] = (i+1) * 2;
-            point = point + 1;
+        for (int i=0; i<30; i++){                       //給予陣列初值
+            num[i] = (i+1) * 2;                         //存入有序資料
+            point = point + 1;                          //游標指示目前位置 (移 1 位)
         }
         print_menu();
         select = keyin.nextInt();
@@ -25,27 +26,27 @@ public class Array_Sequence_excute {
                     break;
                 case 2:
                     if (point >= 50){
-                        System.out.printf("陣列已滿無法插入!!\\n");
+                        System.out.printf("陣列已滿無法插入!!\n");
                     }else {
                         System.out.printf("請輸入欲插入元素 =>");
                         item = keyin.nextInt();
-                        point = point + 1;
-                        k = point;
+                        point = point + 1;                //做移位的動作
+                        k = point;                        //表示原來指標的位置
                         while (true){
-                            if (num[k-1] > item){
-                                num[k] = num[k-1];
-                                k = k -1;
+                            if (num[k-1] > item){         //在陣列插入元素的下方(原本的值)大於我們插入的元素
+                                num[k] = num[k-1];        //做移位的動作
+                                k = k -1;                 //移位完這裡就是換下一個指標
                             }else {
+                                num[k] = item;            //插入元素
                                 break;
                             }
                         }
-                        num[k] = item;
                     }
                     break;
                 case 3:
                     System.out.printf("請輸入想刪除的元素 =>");
-                    item = keyin.nextInt();
-                    int location = Binary_search(item);             //把 location 這個變數新增在 case3當中就好
+                    item = keyin.nextInt();                         //輸入我們要刪除的元素
+                    int location = Binary_search(item);             //把 location 這個變數新增在 case3當中就好, 然後讀 item 的值去尋找有沒有這個元素
                     if (location == -1){                            //如果 location 這個變數是空值
                         System.out.printf("陣列內沒有 %d 元素",item);
                     }else {
@@ -92,22 +93,22 @@ public class Array_Sequence_excute {
 
     //列印刪除元素（二分搜尋法）
     static int Binary_search(int item){
-        int location = -1;
-        int low=0, high=49, mid=0;
-        while ((low+1) < high){
+        int location = -1;                                 //設定空值
+        int low=0, high=49, mid=0;                         //設定原陣列搜尋範圍
+        while ((low+1) < high){                            //陣列是否搜尋完. while((low+1) < high) { ….}』其中 (low+1) < high 表示元素較高的指標 high 還是高於較低的 low，則陣列還未搜尋完畢。
             mid = (low + high) / 2;
-            if (item == num[mid]){
+            if (item == num[mid]){                         //比對陣列中間元素
                 location = mid;
                 break;
             }
             else{
-                if (item > num[mid]){
+                if (item > num[mid]){                      //在陣列的後半段
                     low = mid;
                 }else {
-                    high = mid;
+                    high = mid;                            //在陣列的前半段
                 }
             }
         }
-        return location;
+        return location;                                    //如果有找到位置就要回傳，開始做 case3 的 for迴圈讓位置往前面移。
     }
 }
